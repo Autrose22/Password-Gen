f03fdd8e2e6f2f210f8b159d6ba760fe2f7c44d0
autumnrose <Arosebarrios95@gmail.com>
2021-12-10 18:01:22 -0600

    updating files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,22 @@
+document.querySelector("#generate").addEventListener("click",writePassword);
+
 // Assignment code here
+var special = '!@#$%^&*()+=-[]{}|:<>?,.';
+var numbers = '1234567890';
+var upper =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lower = 'abcdefghijklmnopqrstuvwxyz';
 
+window.onload = alert("Hello! Please click 'Generate password' to get started!")
 
+var confirmSpecialCharacter;
+var confirmNumberCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+function generatePassword() {
+    var confirmLength = prompt("How many characters would you like your password to contain?");
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
